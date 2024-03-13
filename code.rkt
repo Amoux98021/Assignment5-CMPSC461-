0e007951bf249d8d1b63c2407a2f8c52a0ac4fb2
Amoux98021 <107431774+Amoux98021@users.noreply.github.com>
2024-03-13 04:32:27 -0700

    Add files via upload

---
+++ code.rkt
@@ -0,0 +1,26 @@
+#lang racket/base
+
+;problem 1.1
+(define (discount ))
+
+;problem 1.2
+(define (flip ))
+
+;problem 2.1
+(define (list-product))
+
+;problem 2.2
+(define (merge-increasing))
+
+;problem 3.1
+(define (count-lists))
+
+;problem 3.2
+(define (compare-lists))
+
+;problem 4.1
+(define (shadow))
+
+;problem 4.2
+(define (hunt-target))
+        
